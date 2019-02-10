014937dfe2fb91cedf9c5611eddd8b07d517f51a
fanpf <fanpf@cloudcc.com>
2019-02-10 16:09:11 +0800

    代码合并

--- index.js
+++ index.js
@@ -3,23 +3,12 @@ const cors = require('koa2-cors');
 const Koa = require('koa');
 const app = new Koa();
 const router = new Router();
+const bodyParser = require('koa-bodyparser');
 
+app.use(bodyParser())
 const admin = require('./router/admin');
 app.use(cors());
 router.use('/admin',admin)
-<<<<<<< HEAD
-
-router.post('/login',async (ctx,next)=>{
-  let { user,password } = ctx.request.body;
-  let result = await DB.find('tableName',{'user' : user});
-  if(result.length !== 0){
-    ctx.response.body = {status:200,msg:'登陆成功'};
-  }else{
-    ctx.response.body = {status:0,msg:'账号密码错误'};
-  }
-})
-=======
->>>>>>> d42c1252cc3ebdc06ae108974c785e38a7e1cbd1
 
 app.use(router.routes()).use(router.allowedMethods())
 
